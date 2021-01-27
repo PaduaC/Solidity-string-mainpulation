ad5e097148dcad8d85786d63e7e0c468a558fb84
cpadua912 <cristian.padua912@gmail.com>
2021-01-27 16:49:53 -0500

    Added test

--- test/strings.js
+++ test/strings.js
@@ -5,4 +5,14 @@ contract("Strings", () => {
   before(async () => {
     strings = await Strings.deployed();
   });
+
+  it("should return length of string", async () => {
+    const length = await strings.length("cat");
+    assert(length.toNumber() === 3);
+  });
+
+  it("should concatenate two strings", async () => {
+    const concatenatedString = await strings.concatenate("work", "space");
+    assert(concatenatedString === "workspace");
+  });
 });
